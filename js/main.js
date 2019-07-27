69b9b1881007589e42e565cde9d5ac4dfafcf041
AdrianIordache01 <44725298+adrian00s@users.noreply.github.com>
2019-07-27 22:30:09 +0200

    initial files

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// let $input = $('#input');
+// let $submit = $('#send');
+
+// $submit.on('click', request);
+
+// function request(e){
+//     let q = $input.val();
+//     console.log('requested...');
+
+//     $.get(
+//         `https://www.googleapis.com/books/v1/volumes?q=${q}`,{
+//             q: q,
+//             key: "AIzaSyADF0uyRcMqM9biNqbEg6Dd-wBejPwixPg"
+//         },
+//     ).done(function(data){
+//         console.log(data);
+//         console.log("requested successfully")
+//     }).fail(function(){
+//         console.log("theres something wrong with your request")
+//     });
+//     e.preventDefault();
+// }
+
